0d3256531df981d7b597d779158fd1de160db9b4
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-03-01 16:49:24 +0530

    initial

---
+++ file-server/index.js
@@ -0,0 +1,14 @@
+let a=5
+let b= 'String'
+let c=[1,2,3,4,5]
+let d={
+    'value-1': 'This is value 1',
+    'value-2': 'This is value 2'
+}
+
+for(let z=0;z<11;z++)
+{
+    console.log(z)
+}
+
+console.log(a,b,c,d)
